79b5f024a5acd3d860cdf6cb751a1fdcf76152d3
namespace-red <67328203+namespace-red@users.noreply.github.com>
2025-02-18 14:31:19 +0300

    Looped camera mover

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,11 +1,40 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private int _step = 20;
+    [SerializeField] private int _currentPointIndex;
+    [SerializeField] private List<Transform> _points;
+
+    private void OnValidate()
+    {
+        SetIndex(Mathf.Clamp(_currentPointIndex, 0 , _points.Count - 1));
+    }
     
     public void Move(int direction)
     {
-        transform.position += Vector3.right * _step * direction;
+        if ((direction != -1) && (direction != 1))
+        {
+            throw new ArgumentOutOfRangeException(nameof(direction) + direction);
+        }
+    
+        int index = (_currentPointIndex + direction < 0) ? _points.Count - 1 
+            : (_currentPointIndex + direction) % _points.Count;
+    
+        SetIndex(index);
+    }
+    
+    private void SetIndex(int index)
+    {
+        if ((index < 0) || (index >= _points.Count))
+        {
+            throw new IndexOutOfRangeException(nameof(index) + index);
+        }
+    
+        _currentPointIndex = index;
+        
+        transform.position = _points[_currentPointIndex].position;
+        transform.rotation = _points[_currentPointIndex].rotation;
     }
 }
